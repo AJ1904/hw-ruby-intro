5c0a0a76b0b0f1a2dcfa745515425acee1e9174f
AJ1904 <ayushri_jain@tamu.edu>
2023-01-29 18:11:48 -0600

    rubocop suggested updates

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,63 +3,63 @@
 # Part 1
 
 def sum(arr)
-  ans = 0
-  arr.each do |i|
-    ans += i
-  end
-  return ans
+  # ans = 0
+  # arr.each do |i|
+  #   ans += i
+  # end
+  # return ans
+  # one liner ruby
+  arr.sum
 end
 
 def max_2_sum(arr)
-  if (0..2) === arr.length
-    return sum(arr)
-  else
-    first, second = -Float::INFINITY, -Float::INFINITY
-    arr.each do |i|
-      if i >= first
-        first, second = i, first
-      elsif i>=second
-        second = i
-      end
-    end
-  end
-  return first+second
+  # if (0..2) === arr.length
+  #   return sum(arr)
+  # else
+  #   first, second = -Float::INFINITY, -Float::INFINITY
+  #   arr.each do |i|
+  #     if i >= first
+  #       first, second = i, first
+  #     elsif i>=second
+  #       second = i
+  #     end
+  #   end
+  # end
+  # return first+second
+  # sort the array, get the last two elements and return their sum
+  arr.sort.last(2).sum
 end
 
 def sum_to_n?(arr, number)
-  hash = Array.new
+  hash = [] # Array.new
   arr.each do |i|
-    if hash.include?(number - i)
-      return true
-    else
-      hash.push(i)
-    end
+    return true if hash.include?(number - i)
+
+    hash.push(i)
   end
-  return false
+  false
 end
 
 # Part 2
 
 def hello(name)
-  return "Hello, #{name}"
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
   # check if string is empty or does not start with an alphabet
-  if string.empty? || !string[0].match(/[a-z]/i)
-    return false
-  end
+  return false if string.empty? || !string[0].match(/[a-z]/i)
+
   # check if first character is not a consonant
-  return !(string[0]).match(/[aeiou]/i)
+  !(string[0]).match(/[aeiou]/i)
 end
 
 def binary_multiple_of_4?(string)
   # check if string is empty or has any other character than 0 or 1
-  if string.empty? || string.match(/[^01]/)
-    return false
-  end
+  return false if string.empty? || string.match(/[^01]/)
+
   # binary number is multiple of 4 if last two digits are 0
-  return true if string=="0" || string[-2..].match("00")
+  true if string == '0' || string[-2..].match('00')
 end
 
 # Part 3
@@ -69,14 +69,16 @@ class BookInStock
   # tell Ruby to create setters and getters
   attr_accessor :isbn
   attr_accessor :price
+
   def initialize(isbn, price)
-    if isbn.empty? || price<=0
-      raise ArgumentError
-    end
+    raise ArgumentError if isbn.empty? || price <= 0
+
     @isbn = isbn
     @price = price
   end
+
   def price_as_string
-    return '$' + '%0.2f' % price
+    # '$' + '%0.2f' % price
+    format('$%0.2f', @price)
   end
 end
